2e164ae18226fe629a6c6a428f53e141c4b9183a
Steven Leadbeater <info@ledsysconsultancy.com>
2020-03-10 05:44:41 +0000

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,37 @@
+struct ThreeSum {
+    sorted_numbers: Vec<i64>,
+}
+
+impl ThreeSum {
+    fn new(numbers: Vec<i64>) -> ThreeSum {
+        ThreeSum {
+            sorted_numbers: numbers
+        }
+    }
+
+    fn get_triples_sum_to_zero(&self) -> Vec<Vec<i64>> {
+        vec![]
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::ThreeSum;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn new_sorts() {
+        assert_eq!(ThreeSum::new(vec![-1, 0, 1, 2, -1, -4]).sorted_numbers, vec![-4, -1, -1, 0, 1, 2])
+    }
+
+    #[test]
+    fn finds_2() {
+        let sorter = ThreeSum::new(vec![-1, 0, 1, 2, -1, -4]);
+        assert_eq!(sorter.get_triples_sum_to_zero(), vec![vec![-1, -1, 2], vec![-1, 0, 1]]);
+    }
+
+    #[test]
+    fn finds_none() {
+        let sorter = ThreeSum::new(vec![-10, 0, 9, 2, -10, -40]);
+        assert_eq!(sorter.get_triples_sum_to_zero(), vec![]);
     }
 }
